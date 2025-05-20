a5e59dd0a9b663aa14483a9076056987146d93f2
hafid-yahya <yahyahya0404@gmail.com>
2025-05-21 00:05:51 +0700

    Broadcasting EPC Finished

--- public/js/custom-script.js
+++ public/js/custom-script.js
@@ -2,6 +2,9 @@ console.log('hello')
 document.addEventListener('rfid-update', (event) => {
                     // Update form fields
                     console.log('hell')
-                    document.querySelector('input[id="data.epc"]').value = event.detail || '';
-                    
+                    const input = document.querySelector('input[id="data.epc"]')
+
+                    input.value = event.detail || '';
+                    input.dispatchEvent(new Event('input', { bubbles: true }));
+
                 });
